f6882e655af80fbf7677357a58760076a471bfdc
Patricia Montag <post@patricia-montag.de>
2019-12-30 17:39:08 +0100

    Add Step Annotation to make sure Plugin is loaded and described within PDI

--- src/main/java/de/pamo12/steps/TimeConversionStepMeta.java
+++ src/main/java/de/pamo12/steps/TimeConversionStepMeta.java
@@ -1,9 +1,21 @@
 package de.pamo12.steps;
 
+import org.pentaho.di.core.annotations.Step;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.step.*;
 
+
+@Step(
+        id = "TimeConversionStep",
+        name = "TimeConversionStep.Name",
+        description = "TimeConversionStep.TooltipDesc",
+        image = "de/pamo12/steps/resources/timeconversion.svg",
+        categoryDescription = "i18n:org.pentaho.di.trans.step:BaseStep.Category.Transform",
+        i18nPackageName = "de.pamo12.steps",
+        documentationUrl = "TimeConversionStep.DocumentationURL",
+        casesUrl = "TimeConversionStep.CasesURL"
+)
 public class TimeConversionStepMeta  extends BaseStepMeta implements StepMetaInterface {
 
     @Override
